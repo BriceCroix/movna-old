b1a45ea27da54ac5a0a9214ee09a3758fb814dd5
brice croix <brice.croix@gmail.com>
2022-11-26 16:55:24 +0100

    Registered database as injector singleton to avoid bug when catching exceptions.

--- lib/core/data/datasources/local/database_source.dart
+++ lib/core/data/datasources/local/database_source.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'package:injectable/injectable.dart';
 import 'package:isar/isar.dart';
 import 'package:movna/core/data/models/activity_model.dart';
@@ -6,25 +5,15 @@ import 'package:movna/core/data/models/gear_model.dart';
 import 'package:movna/core/data/models/itinerary_model.dart';
 import 'package:movna/core/data/models/sport_converter.dart';
 import 'package:movna/core/domain/entities/activities_filter.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:movna/core/injection.dart';
 
 @Injectable()
 class DataBaseSource {
-  /// Returns Isar schema for the activity models. Must be closed after use.
-  Future<Isar> _openIsar() async {
-    Directory dir = await getApplicationDocumentsDirectory();
-
-    // TODO : register this call as getIt singleton
-    return Isar.open(
-      schemas: [ActivityModelSchema, GearModelSchema, ItineraryModelSchema],
-      directory: dir.path,
-    );
-  }
+  /// Returns global isar instance registered at app startup.
+  Future<Isar> _getIsar() async => injector.getAsync<Isar>();
 
   /// Returns activity models sorted by start time.
   Future<List<ActivityModel>> getActivities([ActivitiesFilter? filter]) async {
-    final isar = await _openIsar();
-
     List<WhereClause> whereClauses = [];
     List<FilterCondition> filterConditions = [];
 
@@ -66,28 +55,24 @@ class DataBaseSource {
       }
     }
 
-    final query = isar.activityModels.buildQuery<ActivityModel>(
-      whereClauses: whereClauses,
-      filter: FilterGroup.and(filterConditions),
-      sortBy: [
-        const SortProperty(
-          property: ActivityModel.propertyStartTimeName,
-          sort: Sort.desc,
-        )
-      ],
-      limit: filter?.maxCount,
-      offset: 0,
-    );
+    final query = (await _getIsar()).activityModels.buildQuery<ActivityModel>(
+          whereClauses: whereClauses,
+          filter: FilterGroup.and(filterConditions),
+          sortBy: [
+            const SortProperty(
+              property: ActivityModel.propertyStartTimeName,
+              sort: Sort.desc,
+            )
+          ],
+          limit: filter?.maxCount,
+          offset: 0,
+        );
 
-    final res = query.findAll();
-
-    isar.close();
-
-    return res;
+    return query.findAll();
   }
 
   Future<void> saveActivityToDatabase(ActivityModel model) async {
-    final isar = await _openIsar();
+    final isar = await _getIsar();
     await isar.writeTxn((isar) async {
       // putSync takes care of saving all links, but let's keep it async
 
@@ -99,88 +84,69 @@ class DataBaseSource {
       }
       await isar.activityModels.put(model, saveLinks: true);
     });
-    isar.close();
   }
 
   Future<void> removeActivityFromDatabase(ActivityModel model) async {
-    final isar = await _openIsar();
+    final isar = await _getIsar();
     await isar.writeTxn((isar) async {
       await isar.activityModels.delete(model.id);
     });
-    isar.close();
   }
 
   /// Returns gear models sorted by name.
   Future<List<GearModel>> getGear([int? maxCount]) async {
-    final isar = await _openIsar();
+    final isar = await _getIsar();
 
     Future<List<GearModel>> res = maxCount != null
         ? isar.gearModels.where().sortByName().limit(maxCount).findAll()
         : isar.gearModels.where().sortByName().findAll();
-
-    isar.close();
-
     return res;
   }
 
   Future<void> saveGearToDatabase(GearModel model) async {
-    final isar = await _openIsar();
+    final isar = await _getIsar();
     await isar.writeTxn((isar) async {
       await isar.gearModels.put(model);
     });
-    isar.close();
   }
 
   Future<void> removeGearFromDatabase(GearModel model) async {
-    final isar = await _openIsar();
+    final isar = await _getIsar();
     await isar.writeTxn((isar) async {
       await isar.gearModels.delete(model.id);
     });
-    isar.close();
   }
 
   /// Returns number of gear models in database.
-  Future<int> getGearCount() async {
-    final isar = await _openIsar();
-    int count = await isar.gearModels.where().count();
-    isar.close();
-    return count;
-  }
+  Future<int> getGearCount() async =>
+      await (await _getIsar()).gearModels.where().count();
 
   /// Returns itinerary models sorted by name.
   Future<List<ItineraryModel>> getItineraries([int? maxCount]) async {
-    final isar = await _openIsar();
+    final isar = await _getIsar();
 
     Future<List<ItineraryModel>> res = maxCount != null
         ? isar.itineraryModels.where().sortByName().limit(maxCount).findAll()
         : isar.itineraryModels.where().sortByName().findAll();
 
-    isar.close();
-
     return res;
   }
 
   Future<void> saveItineraryToDatabase(ItineraryModel model) async {
-    final isar = await _openIsar();
+    final isar = await _getIsar();
     await isar.writeTxn((isar) async {
       await isar.itineraryModels.put(model);
     });
-    isar.close();
   }
 
   Future<void> removeItineraryFromDatabase(ItineraryModel model) async {
-    final isar = await _openIsar();
+    final isar = await _getIsar();
     await isar.writeTxn((isar) async {
       await isar.itineraryModels.delete(model.id);
     });
-    isar.close();
   }
 
   /// Returns number of itineraries stored in database.
-  Future<int> getItinerariesCount() async {
-    final isar = await _openIsar();
-    int count = await isar.itineraryModels.where().count();
-    isar.close();
-    return count;
-  }
+  Future<int> getItinerariesCount() async =>
+      await (await _getIsar()).itineraryModels.where().count();
 }
--- lib/core/injection.dart
+++ lib/core/injection.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:isar/isar.dart';
+import 'package:movna/core/data/models/activity_model.dart';
+import 'package:movna/core/data/models/gear_model.dart';
+import 'package:movna/core/data/models/itinerary_model.dart';
+import 'package:path_provider/path_provider.dart';
 import 'injection.config.dart';
 
 final injector = GetIt.instance;
@@ -9,7 +16,19 @@ final GlobalKey<NavigatorState> _navigatorKey = GlobalKey();
 @InjectableInit()
 GetIt _configureDependencies() => $initGetIt(injector);
 
-void setUpInjector() {
+Future<Isar> _openIsar() async {
+  Directory dir = await getApplicationDocumentsDirectory();
+  return Isar.open(
+    schemas: [ActivityModelSchema, GearModelSchema, ItineraryModelSchema],
+    directory: dir.path,
+  );
+}
+
+void setUpInjector() async {
   _configureDependencies();
   injector.registerSingleton<GlobalKey<NavigatorState>>(_navigatorKey);
+  injector.registerLazySingletonAsync<Isar>(
+    () async => await _openIsar(),
+    dispose: (isar) => isar.close(),
+  );
 }
